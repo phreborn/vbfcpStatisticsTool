9cd11ad48b3bf00c43f1e576d3b9e831d79236ae
Stefan Gadatsch <stefan.gadatsch@gmail.com>
2017-01-21 22:37:03 +0100

    Compiling
    - Compile the fitting code

---
+++ LinkDef.h
@@ -0,0 +1,15 @@
+#include "ExtendedModel.hxx"
+#include "ExtendedMinimizer.hxx"
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ class ExtendedModel+;
+#pragma link C++ class ExtendedMinimizer+;
+
+#endif // __CINT__
